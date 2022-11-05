9967b7169528c09bdb1dd7f8daaaeb735e2e21d3
Bui Duc <84183983+DucOriginal@users.noreply.github.com>
2022-11-06 04:09:00 +0700

    Add files via upload

---
+++ fsm_mode.h
@@ -0,0 +1,22 @@
+/*
+ * fsm_mode.h
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: Duc Original
+ */
+
+#ifndef INC_FSM_MODE_H_
+#define INC_FSM_MODE_H_
+
+#include "main.h"
+#include "button.h"
+
+#define STATE_1 1
+#define STATE_2 2
+#define STATE_3 3
+
+extern int status;
+void fsm_mode_run();
+
+
+#endif /* INC_FSM_MODE_H_ */
